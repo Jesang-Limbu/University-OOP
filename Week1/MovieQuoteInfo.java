421441681de933f3bd7b38fdd77fbd705b3f6faf
Jesang-Limbu <LimbuJesang@gmail.com>
2025-10-12 19:55:08 +0100

    Add Week1 directory Add Week1 MovieQuoteInfo.java

---
+++ Week1/MovieQuoteInfo.java
@@ -0,0 +1,12 @@
+/* Java Programming - Joyce Farrell
+   Chapter 1 Exercise 7
+*/
+
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("This is the beginning of a beautiful friendship.");
+        System.out.println("Cassablanca");
+        System.out.println("Rick Blaine");
+        System.out.println("1942");
+    }
+}
