25499b494ecedd001748ebbc27824b8da0a6ced2
Jesang-Limbu <LimbuJesang@gmail.com>
2025-10-13 10:04:12 +0100

    Add Week1/BurmaShave.java
    Signed-off-by: Jesang-Limbu <LimbuJesang@gmail.com>

---
+++ Week1/BurmaShave.java
@@ -0,0 +1,22 @@
+/*
+  Java Programming - Joyce Farrell
+  Chapter 1 Exercise 11
+*/
+
+public class BurmaShave {
+    public static void main(String args[]) {
+	String line1 = "Every shaver";
+	String line2 = "Now can snore";
+	String line3 = "Six more minutes";
+	String line4 = "Than before";
+	String line5 = "By using";
+	String line6 = "Burma Shave";
+
+	System.out.println(line1);
+	System.out.println(line2);
+	System.out.println(line3);
+	System.out.println(line4);
+	System.out.println(line5);
+	System.out.println(line6);
+    }
+}
