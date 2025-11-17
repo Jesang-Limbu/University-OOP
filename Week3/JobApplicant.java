42a111e1b7b75054d260ae8b1e626feaa8846fe6
Jesang-Limbu <LimbuJesang@gmail.com>
2025-11-17 11:11:06 +0000

    Complete JobApplicant.java
    - Add constructor for class JobApplicant
    - Add get methods for class JobApplicamt

---
+++ Week3/JobApplicant.java
@@ -0,0 +1,34 @@
+public class JobApplicant {
+    String name;
+    String phoneNumber;
+    boolean wordProcess, spreadsheet, database, graphics;
+
+    public JobApplicant(String name, String phoneNumber, boolean wordProcess, boolean spreadsheet, boolean database, boolean graphics) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.wordProcess = wordProcess;
+        this.spreadsheet = spreadsheet;
+        this.database = database;
+        this.graphics = graphics;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public boolean canWordProcess() {
+        return wordProcess;
+    }
+
+    public boolean canSpreadsheet() {
+        return spreadsheet;
+    }
+
+    public boolean canDatabase() {
+        return database;
+    }
+}
